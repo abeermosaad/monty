23d1bffe3cbcad9ee80e3dfebfff4d87f619dcc5
abeermosaad <abeermosaad69@gmail.com>
2023-06-22 22:56:51 +0300

    add opcode

--- intructions3.c
+++ intructions3.c
@@ -19,3 +19,28 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = num;
 }
+
+/**
+ * add - .
+ * @stack: .
+ * @line_number: .
+ * Return: .
+*/
+void add(stack_t **stack, unsigned int line_number)
+{
+	int num;
+	stack_t *temp;
+
+	if (!(*stack) || !((*stack)->next))
+	{
+		free_list(stack);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	num = (*stack)->n;
+	(*stack)->next->n += num;
+	(*stack)->next->prev = NULL;
+	free(temp);
+	*stack = (*stack)->next;
+}
--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
 	unsigned int count = 0;
 	int idx;
 	instruction_t opcode[] = {{"push", push}, {"pall", pall}, {"pint", pint},
-	{"pop", pop}, {"swap", swap}};
+	{"pop", pop}, {"swap", swap}, {"add", add}};
 	stack_t *stack = NULL;
 
 	if (argc != 2)
