30b9323de2f54a4fea9c0ebe28c0b6d2da003abb
abeermosaad <abeermosaad69@gmail.com>
2023-06-22 19:33:25 +0300

    handle pop error

--- instructions.c
+++ instructions.c
@@ -99,7 +99,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!(*stack))
 	{
 		free_list(stack);
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->next == NULL)
