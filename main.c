8dc02f690e9baf96c3927e83588eef899c7db7a7
Abodawoud <yousefabodawoud@gmail.com>
2023-06-22 16:41:21 +0300

    test

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	if (file == NULL)
+	if (file == NULL || access(argv[1], X_OK))
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
