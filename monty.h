f8660e47217688cad9b07934eebd4c484f3335f2
Abodawoud <yousefabodawoud@gmail.com>
2023-06-22 16:21:03 +0300

    betty h

--- monty.h
+++ monty.h
@@ -19,9 +19,9 @@ extern char *value;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 
@@ -35,8 +35,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 char *handle_new_line(char *line);
