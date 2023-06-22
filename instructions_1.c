9561be53ecf215a36d48d6a561ac8ec1b85f4790
Abodawoud <yousefabodawoud@gmail.com>
2023-06-22 23:45:09 +0300

    again

--- instructions_1.c
+++ instructions_1.c
@@ -14,7 +14,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n >= 128)
+	if ((*stack)->n < 0 || (*stack)->n >= 127)
 	{
 		free_list(stack);
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
