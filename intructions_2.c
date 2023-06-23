485b3994742b8be2fe3f6f28434deff260337304
abeermosaad <abeermosaad69@gmail.com>
2023-06-23 16:40:12 +0300

    rotr opcode

--- intructions_2.c
+++ intructions_2.c
@@ -33,7 +33,7 @@ void rotl(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number;
 
-	if((*stack) && ((*stack)->next))
+	if ((*stack) && ((*stack)->next))
 	{
 		num = (*stack)->n;
 		while (current->next != NULL)
@@ -45,4 +45,30 @@ void rotl(stack_t **stack, unsigned int line_number)
 	}
 }
 
+/**
+ * rotr - .
+ * @stack: .
+ * @line_number: .
+ * Return: .
+*/
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	int num;
+	stack_t *current = *stack;
+	(void)line_number;
 
+	if ((*stack) && ((*stack)->next))
+	{		
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		num = current->n;
+		while (current != (*stack))
+		{
+			current->n = current->prev->n;
+			current = current->prev;
+		}
+		(*stack)->n = num;
+	}
+}
--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
 	instruction_t opcode[] = {{"push", push}, {"pall", pall}, {"pint", pint},
 	{"pop", pop}, {"swap", swap}, {"add", add}, {"pchar", pchar}, {"nop", nop},
 	{"sub", sub}, {"div", div1}, {"mul", mul}, {"mod", mod}, {"pstr", pstr},
-	{"rotl", rotl}};
+	{"rotl", rotl}, {"rotr", rotr}};
 	stack_t *stack = NULL;
 
 	if (argc != 2)
